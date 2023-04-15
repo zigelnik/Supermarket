42db346080a2b49f3ac669e317bfa62a8e5a2fb8
Tal Zigelnik <113368830+xxxlr1@users.noreply.github.com>
2023-04-15 15:19:02 +0300

    Add files via upload

---
+++ Headers/classes.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Headers/Market.h"
+#include "Headers/F_Vegetable.h"
+#include "Headers/F_Fruit.h"
+#include "Headers/D_Drink.h"
+#include "Headers/D_Cheese.h"
+#include "Headers/D_Other.h"
+#include "Headers/D_Yogurt.h"
+#include "Headers/Pack.h"
+using namespace std;
